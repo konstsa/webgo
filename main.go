a6d0d8e7c2b7a36b96d3bc4408bd3793cbd518d6
Konstantin Sosnitskiy <konstsa@gmail.com>
2017-07-26 12:56:20 +0300

    cnah

--- main.go
+++ main.go
@@ -1,29 +1,25 @@
 package main
 
 import (
-	"net/http"
 	"html/template"
-
-
+	"net/http"
 )
 
 type page struct {
-	Title string	//имена переменных должны совпадать с тем, что мы написали выше!
-	Msg string		//и переменные обязательно должны быть публичными!
+	Title string //имена переменных должны совпадать с тем, что мы написали выше!
+	Msg   string //и переменные обязательно должны быть публичными!
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-type", "text/html")
 
-
-	t, _ :=template.ParseFiles("templates/index.html")
+	t, _ := template.ParseFiles("/index.html")
 	t.Execute(w, &page{Title: "Just page", Msg: "Hello World"})
-
 
 }
 
 func main() {
 
 	http.HandleFunc("/", index)
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":80", nil)
 }
